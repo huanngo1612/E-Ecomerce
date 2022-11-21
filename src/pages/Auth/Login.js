e09384d13d6f0dbd80b7a3ec1fe4c6026f3fb4fb
huanngo012 <huanngo1612@gmail.com>
2022-11-21 12:46:23 +0700

    update

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,5 +1,33 @@
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import loginImg from "../../assets/images/login.png";
+import "../Auth/Auth.scss";
 function Login() {
-  return <></>;
+  return (
+    <section className="auth">
+      <div className="login-img">
+        <img src={loginImg} alt="" />
+      </div>
+      <div className="form">
+        <h2>Login</h2>
+        <form>
+          <input type="text" placeholder="Email" required />
+          <input type="password" placeholder="Password" required />
+          <button text>Login</button>
+          <p>--or--</p>
+        </form>
+        <button className="btn-google">
+          <FontAwesomeIcon icon={faGoogle}></FontAwesomeIcon>Login with Google
+        </button>
+        <span>
+          <p>
+            Don't you have an account ?<Link to="/register"> Register</Link>
+          </p>
+        </span>
+      </div>
+    </section>
+  );
 }
 
 export default Login;
--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
+import registerImg from "../../assets/images/register.png";
+import "../Auth/Auth.scss";
 function Register() {
-  return <></>;
+  return (
+    <section className="auth">
+      <div className="register-img">
+        <img src={registerImg} alt="" />
+      </div>
+      <div className="form">
+        <h2>Register</h2>
+        <form>
+          <input type="text" placeholder="Email" required />
+          <input type="password" placeholder="Password" required />
+          <input type="password" placeholder="Confirm Password" required />
+          <button text>Register</button>
+        </form>
+        <span>
+          <p>
+            {" "}
+            You have an account ? <Link to="/login"> Login</Link>
+          </p>
+        </span>
+      </div>
+    </section>
+  );
 }
 
 export default Register;
